d08c3ded18d45de9d6d54fde0c3459a6e630aee3
vepa83 <vepaorazov83@gmail.com>
2020-02-25 20:55:37 +0500

    I have deleted all files

--- items/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . models import Item, Location, Image, Like, Footer, Comment
-
-admin.site.register(Item)
-admin.site.register(Location)
-admin.site.register(Image)
-admin.site.register(Like)
-admin.site.register(Footer)
-admin.site.register(Comment)
